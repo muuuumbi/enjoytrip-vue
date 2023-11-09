c67fbab39843e63cd43afd61ee525bcba61d01b1
박세헌 <quwe153@naver.com>
2023-11-09 16:48:19 +0900

    first commit

---
+++ src/components/TheLogin.vue
@@ -0,0 +1,44 @@
+<script setup></script>
+
+<template>
+  <div class="login-wrapper">
+    <h2>Login</h2>
+    <form method="post" id="login-form">
+      <input type="text" name="userId" placeholder="ID" />
+      <input type="text" name="userPwd" placeholder="PASSWORD" />
+      <input type="submit" value="Login" />
+    </form>
+  </div>
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+  border: none;
+}
+div {
+  font-size: 14px;
+  font-family: "Roboto", sans-serif;
+}
+.login-wrapper {
+  width: 400px;
+  height: 350px;
+  padding: 40px;
+  box-sizing: border-box;
+}
+.login-wrapper > h2 {
+  font-size: 24px;
+  color: rgb(76, 137, 190);
+  margin-bottom: 20px;
+}
+#login-form > input {
+  width: 100px;
+  height: 48px;
+  padding: 0 10px;
+  box-sizing: border-box;
+  margin-bottom: 16px;
+  border-radius: 6px;
+  background-color: white;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheLoginVue from "../components/TheLogin.vue";
+</script>
+
+<template>
+  <TheLoginVue></TheLoginVue>
+</template>
+
+<style scoped></style>
